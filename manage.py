76d192353d8c4d0946f1495e61c1a0ab50ff2179
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2021-07-16 17:36:32 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,9 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bakeryManagement.settings')
+    
+    credit_card_number = " khsdf" 
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
